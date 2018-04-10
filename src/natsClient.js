c51fab5891a065063beaea526bf6440c61104aec
Joris Röling <joris@bonboa.com>
2018-04-10 13:17:14 +0200

    v0.0.17

--- src/natsClient.js
+++ src/natsClient.js
@@ -1,8 +1,9 @@
 const NATS = require('nats');
+NATS.MAX_CONTROL_LINE_SIZE = 1024
+
 class natsClient {
   constructor(){
     this.client = NATS.connect({ maxReconnectAttempts: -1, reconnectTimeWait: 250, waitOnFirstConnect: true, reconnect: true })
-    if (this.client) this.client.MAX_CONTROL_LINE_SIZE = 1024; //(or match server or any custom size you provide to the server.)
   }
 
   send(payload) {
